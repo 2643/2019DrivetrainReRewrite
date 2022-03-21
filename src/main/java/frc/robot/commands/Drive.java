8ef1d69cdef10c4af102408740f345552f2467dc
Vikram <vikram@nandi.net>
2022-03-21 16:43:47 -0700

    first commit

---
+++ src/main/java/frc/robot/commands/Drive.java
@@ -0,0 +1,52 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.Constants;
+import frc.robot.RobotContainer;
+
+public class Drive extends CommandBase {
+  double leftSpeed;
+  double rightSpeed;
+  /** Creates a new Drive. */
+  public Drive() {
+    // Use addRequirements() here to declare subsystem dependencies.
+    addRequirements(RobotContainer.m_DriveTrain);
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {}
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    if (Math.abs(RobotContainer.driveStick.getRawAxis(Constants.JOYSTICK_LEFT_AXIS)) > Constants.JOYSITCK_DEADBAND) {
+      leftSpeed = (RobotContainer.driveStick.getRawAxis(Constants.JOYSTICK_LEFT_AXIS));
+    }
+    else{
+      leftSpeed = 0;
+    }
+    if (Math.abs(RobotContainer.driveStick.getRawAxis(Constants.JOYSTICK_RIGHT_AXIS)) > Constants.JOYSITCK_DEADBAND) {
+      rightSpeed = (RobotContainer.driveStick.getRawAxis(Constants.JOYSTICK_RIGHT_AXIS));
+    }
+    else {
+      rightSpeed = 0;
+    }
+    RobotContainer.m_DriveTrain.setLeftSpeed(leftSpeed);
+    RobotContainer.m_DriveTrain.setRightSpeed(rightSpeed);
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {}
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
---
+++ src/main/java/frc/robot/subsystems/DriveTrain.java
@@ -0,0 +1,51 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class DriveTrain extends SubsystemBase {
+  public static int lFrontMotorPort = 29;   
+  public static int lBackMotorPort = 21;    
+  public static int rFrontMotorPort = 26;   
+  public static int rBackMotorPort = 38;
+  public static TalonSRX LeftFrontMotor = new TalonSRX(lFrontMotorPort);
+  public static TalonSRX LeftBackMotor = new TalonSRX(lBackMotorPort);
+  public static TalonSRX RightFrontMotor = new TalonSRX(rFrontMotorPort);
+  public static TalonSRX RightBackMotor = new TalonSRX(rBackMotorPort);
+  /** Creates a new DriveTrain. */
+  public DriveTrain() 
+  {
+        LeftFrontMotor.configContinuousCurrentLimit(32, 0);
+        LeftFrontMotor.configPeakCurrentLimit(35, 0);
+        LeftFrontMotor.configPeakCurrentDuration(80, 0);
+        LeftFrontMotor.enableCurrentLimit(true);
+
+        LeftBackMotor.follow(LeftFrontMotor);
+        RightBackMotor.follow(RightFrontMotor);
+
+        RightFrontMotor.configContinuousCurrentLimit(32, 0);
+        RightFrontMotor.configPeakCurrentLimit(35, 0);
+        RightFrontMotor.configPeakCurrentDuration(80, 0);
+        RightFrontMotor.enableCurrentLimit(true);
+
+  }
+
+  public void setLeftSpeed(double speed) {
+    LeftFrontMotor.set(ControlMode.PercentOutput, -speed);
+}
+
+public void setRightSpeed(double speed) {
+  RightFrontMotor.set(ControlMode.PercentOutput, speed);
+}
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+}
